9a6ed1dc9786a37b4ed51168282488b8b9d7556f
ilham-sh <ilhamsyauqih@outlook.co.id>
2025-09-20 12:25:53 +0700

    Update 1 - Navbar Section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quam iure asperiores illum culpa iste ratione maiores reiciendis maxime error magnam blanditiis ipsam mollitia accusantium, dolore, beatae itaque vel illo aliquid libero odio numquam laudantium quibusdam? Ea ipsa iure maiores quod voluptatem sint dolorum quo. Pariatur perspiciatis eos exercitationem quas earum fugit amet, adipisci similique provident vel! Aperiam quibusdam quaerat a consequatur at et, nesciunt, sed tempore hic maxime cumque? Non nulla aspernatur ut laudantium eveniet. Dolore repellendus ullam repudiandae enim accusamus, doloribus praesentium nesciunt qui ipsum quo. Et porro asperiores voluptatibus veniam architecto unde nobis quibusdam magnam dignissimos nostrum recusandae ab nihil, laboriosam aliquid, fuga explicabo. Amet reiciendis nam cupiditate aut quo odit dignissimos dolorum animi commodi. Harum eos soluta suscipit doloremque eaque maiores dolor ex iste architecto labore hic sunt, reiciendis perspiciatis quis? Dolorem maiores quisquam voluptas nisi enim nulla ipsum quas natus ratione incidunt impedit excepturi cupiditate itaque voluptatibus tenetur eius reprehenderit, expedita facere saepe repudiandae soluta recusandae quidem blanditiis unde? Repellat assumenda, maiores amet soluta officia explicabo dolorem. At quam, molestiae voluptas laborum voluptatem quod dicta non tempore odio iusto, autem tempora in nobis placeat asperiores saepe commodi qui doloribus assumenda labore nisi sit. Numquam, a?
+      </h1>
+    </>
+  )
+}
+
+export default App
